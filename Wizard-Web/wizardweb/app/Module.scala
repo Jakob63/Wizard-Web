b33fccc9eb5e32dbc2338fb1b1859e9c88d19092
jakob63 <jakobzim20002@gmail.com>
2026-01-06 15:18:16 +0100

    new heroku branch

--- Wizard-Web/wizardweb/app/Module.scala
+++
@@ -1,8 +0,0 @@
-import com.google.inject.AbstractModule
-import util.{QueueInput, UserInput}
-
-class Module extends AbstractModule {
-  override def configure(): Unit = {
-    bind(classOf[UserInput]).to(classOf[QueueInput])
-  }
-}
