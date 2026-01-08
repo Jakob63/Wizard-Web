b6fb48b843112ce292dbb68e8eeda45baa2b5231
el871bra <el871bra@htwg-konstanz.de>
2026-01-08 09:39:13 +0100

    deploy fix

--- wizardweb/frontend/src/router.js
+++ wizardweb/frontend/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import IndexPage from './components/IndexPage.vue';
 import HomePage from './components/HomePage.vue';
@@ -22,7 +22,7 @@ const routes = [
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 });
 
